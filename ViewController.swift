6acf2d3dc370042d60f4915eea005ddcb15c3717
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-12-31 02:26:41 -0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aven_CodingExercise
+//
+//  Created by Jenny Morales on 12/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
